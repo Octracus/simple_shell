d48c82a919ef35bd5a9346266bc346ea7462c26d
Octracus <julianannor1@gmail.com>
2023-12-14 04:00:07 +0000

    Shell stuff

---
+++ memory.c
@@ -0,0 +1,19 @@
+78% of storage used … You use storage when you save to Drive, back up to Google Photos, and send and receive email on Gmail
+#include "shell.h"
+
+/**
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
